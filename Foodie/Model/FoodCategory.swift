0fa48feb8f4f520dd96fca5366b2e36e0a53a90f
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-04-20 12:36:47 +0200

    Project start

---
+++ Foodie/Model/FoodCategory.swift
@@ -0,0 +1,13 @@
+//
+//  FoodCategory.swift
+//  Foodie
+//
+//  Created by Özgün Yildiz on 20.04.21.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ Foodie/Model/RecipeModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeModel.swift
+//  Foodie
+//
+//  Created by Özgün Yildiz on 20.04.21.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
